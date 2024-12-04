6dfb35fb1e0dea9591118721ec299b5c3ede17eb
Keith <kpost342005@hotmail.com>
2024-12-03 19:11:05 -0500

    Replaced seaborn code of '_shiny()' versions of fns with plotly express code as they rendered better

--- code/_00_power_fns.py
+++ code/_00_power_fns.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import plotly.express as px
 
 
 
@@ -131,23 +131,37 @@ def make_hist_shiny(df, var, year=pd.NA, lift=pd.NA, col='darkblue'):
   #filter by year if populated
   if pd.notna(year):
     df = df[df['year']==year]
-  
+
   #convert to DF of unique records of variable
   if var=='mass_kg':
     df1 = df[df['lift']==lift]
+    labx = lift + ' (kg)'
   else:
     df1 = df[['id', var]].drop_duplicates()
+    labx = var
 
-  #plot histogram
-  sns.histplot(x=var, color=col, data=df1)
-  plt.xlabel(var, fontsize=14)
-  plt.ylabel('Count', fontsize=14)
+  #create histogram using plotly express
+  fig = px.histogram(x=var, color_discrete_sequence=[col], data_frame=df1,
+                     labels={var: labx})
+  
+  #add edges to histogram bars
+  fig.update_traces(marker_line_width=1, marker_line_color="black")
+  
+  #customize layout
+  fig.update_layout(
+    autosize=True,  
+    height=None, 
+    width=None,
+    margin={"l": 5, "r": 5, "t": 27, "b": 5}
+  )
+
+  return fig
   
   
 ## Scatterplot
 ## Make scatterplot
 def make_scatter_shiny(df, year=pd.NA, varx='mass_kg', vary='mass_kg', liftx=pd.NA, lifty=pd.NA, 
-                 pt_col='darkblue', line_col='green'):
+                       pt_col='darkblue', line_col='green'):
   #filter DF by year if populated
   if pd.notna(year):
     df = df[df['year']==year]
@@ -156,42 +170,65 @@ def make_scatter_shiny(df, year=pd.NA, varx='mass_kg', vary='mass_kg', liftx=pd.
   if varx=='mass_kg' and vary=='mass_kg':
     df1 = df[df['lift'].isin([liftx, lifty])]
     df2 = df1[['id', 'lift', 'mass_kg']].pivot(index='id', columns='lift', values='mass_kg')
+  
+    fig = px.scatter(x=liftx, y=lifty, data_frame=df2,
+                     color_discrete_sequence=[pt_col],
+                     opacity=0.2,
+                     trendline='ols',
+                     trendline_scope='overall',
+                     trendline_color_override=line_col,
+                     labels={varx: labx, vary: laby})
     
-    sns.lmplot(x=liftx, y=lifty, ci=95, data=df2,
-             scatter_kws={'color': pt_col,
-                          'alpha': 0.2},
-             line_kws={'color': line_col})
-    plt.xlabel(liftx, fontsize=14)
-    plt.ylabel(lifty, fontsize=14)
-    plt.show()
-    plt.close()
+    #customize layout
+    fig.update_layout(
+      autosize=True,  
+      height=None, 
+      width=None,
+      margin={"l": 5, "r": 5, "t": 27, "b": 5},
+      showlegend=False
+    )
+    
   #scenarios 2-4: either 0 or 1 var is 'mass_kg'
   else:
     if varx=='mass_kg':
       df1 = df[df['lift']==liftx]
       df2 = df1[['id', varx, vary]]
-      labx = liftx
+      labx = liftx + ' (kg)'
       laby = vary
     elif vary=='mass_kg':
       df1 = df[df['lift']==lifty]
       df2 = df1[['id', varx, vary]]
-      labx = varx
-      laby = lifty
+      if varx == 'age':
+        labx = varx + ' (yr)'
+      else:
+        labx = varx
+      laby = lifty + ' (kg)'
     else:
       df2 = df[['id', varx, vary]].drop_duplicates()
       labx = varx
       laby = vary
-      
-    sns.lmplot(x=varx, y=vary, ci=95, data=df2,
-               scatter_kws={'color': pt_col,
-                            'alpha': 0.2},
-               line_kws={'color': line_col})
-    plt.xlabel(labx, fontsize=14)
-    plt.ylabel(laby, fontsize=14)
-
+    
+    fig = px.scatter(x=varx, y=vary, data_frame=df2,
+                     color_discrete_sequence=[pt_col],
+                     opacity=0.2,
+                     trendline='ols',
+                     trendline_scope='overall',
+                     trendline_color_override=line_col,
+                     labels={varx: labx, vary: laby})
+    
+    #customize layout
+    fig.update_layout(
+      autosize=True,  
+      height=None, 
+      width=None,
+      margin={"l": 5, "r": 5, "t": 27, "b": 5},
+      showlegend=False
+    )
+               
+  return fig
 
 ## Boxplot
-def make_boxplot_shiny(df, varx, vary='mass_kg', lifty=pd.NA, year=pd.NA):
+def make_boxplot_shiny(df, varx, vary='mass_kg', lifty=pd.NA, year=pd.NA, col=pd.NA):
   #filter DF by year if populated
   if pd.notna(year):
     df = df[df['year']==year]
@@ -199,14 +236,24 @@ def make_boxplot_shiny(df, varx, vary='mass_kg', lifty=pd.NA, year=pd.NA):
   if vary=='mass_kg':
     df1 = df[df['lift']==lifty]
     df2 = df1[['id', varx, vary]]
-    laby = lifty + '_kg'
+    laby = lifty + ' (kg)'
   else:
     df2 = df[['id', varx, vary]].drop_duplicates()
     laby = vary
-    
-  sns.boxplot(x=varx, y=vary, data=df2)
-  plt.xlabel(varx, fontsize=14)
-  plt.ylabel(laby, fontsize=14)
+  
+  fig=px.box(data_frame=df2, x=varx, y=vary,
+             color_discrete_sequence=[col],
+             labels={varx: varx, vary: laby})
+             
+  #customize layout
+  fig.update_layout(
+    autosize=True,  
+    height=None, 
+    width=None,
+    margin={"l": 5, "r": 5, "t": 27, "b": 5}
+  )
+  
+  return fig
 
 
 
